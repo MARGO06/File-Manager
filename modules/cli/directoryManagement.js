a16b81f0b807db78e7d75a0a35240e3fb7ac850d
Margarita Masko <margaritam996@gmail.com>
2025-02-05 19:34:06 +0300

    doc: add a project's description

--- modules/cli/directoryManagement.js
+++ modules/cli/directoryManagement.js
@@ -1,5 +1,3 @@
 export const pathToWorkingDirectory = (currentDirectory) => {
-  process.stdout.write(
-    `You are currently in ${currentDirectory}\nEnter your command:`
-  );
+  process.stdout.write(`You are currently in ${currentDirectory}\n> `);
 };
